efc16e5813fac579504d58e7e8c340e9a452d4b3
dennis <dennis@dennis.com>
2018-04-30 22:36:40 +0800

    all works except work_experience

--- src/veryeast.py
+++ src/veryeast.py
@@ -100,7 +100,7 @@ class VeryEast(object):
                             baseinfo['IDCard']=value
                 preview['baseinfo']=baseinfo
                 #下面的详细内容
-                preview_main_list=resume_preview.xpath('div[@class="resume_preview_main"]')
+                preview_main_list=resume_preview.xpath('div[contains(@class,"resume_preview_main")]')
                 for main in preview_main_list:
                     title=main.xpath('h1/text()')[0]
                     if(title=='最近一份工作'):
@@ -115,20 +115,182 @@ class VeryEast(object):
                             if(len(detail.strip())<=0):
                                 continue
                             k,v=detail.replace(' ','').replace('：',':').split(':')
+                            k=k.replace(u'\u3000',u'').replace(u'\xa0',u'')
+                            v=v.replace(u'\u3000',u'').replace(u'\xa0',u'')
                             if(k=='企业性质'):
                                 lastest_work['nature']=v
                             elif(k=='职位'):
                                 lastest_work['job_title']=v
                             else:
-                                raise Exception('最近一份工作-有未处理的字段，地址：%s'%self.previewUrl.format(data["user_id"]))
-                        print(lastest_work)
+                                raise Exception('最近一份工作-有未处理的字段，地址：{0}'.format(self.previewUrl.format(data["user_id"])))
+                        preview['lastest_work']=lastest_work
                     elif(title=='求职意向'):
                         job_intension={}
+                        intensions=main.xpath('ul/li/text()')
+                        if(len(intensions)<=0):
+                            continue
+                        for intension in intensions:
+                            k,v=intension.replace(' ','').replace('：',':').split(':')
+                            k=k.replace(u'\u3000',u'').replace(u'\xa0',u'')
+                            v=v.replace(u'\u3000',u'').replace(u'\xa0',u'')
+                            if(k=='到岗时间'):
+                                job_intension['AvailableReportDuty']=v
+                            elif(k=='工作类型'):
+                                job_intension['WorkType']=v
+                            elif(k=='工作地点'):
+                                job_intension['WorkPlace']=v
+                            elif(k=='意向职位'):
+                                job_intension['IntentJob']=v
+                            elif(k=='意向企业'):
+                                job_intension['IntentCompany']=v
+                            elif(k=='期望薪资'):
+                                job_intension['IntentMoney']=v
+                        preview['job_intension']=job_intension
                     elif(title=='工作经验'):
+
                         pass
+                    elif(title=='语言能力'):
+                        language_info={}
+                        languages=main.xpath('ul/li/text()')
+                        if(len(languages)<=0):
+                            continue
+                        for language in languages:
+                            k,v=language.replace(' ','').replace('：',':').split('\u3000')
+                            k=k.replace(u'\u3000',u'').replace(u'\xa0',u'')
+                            v=v.replace(u'\u3000',u'').replace(u'\xa0',u'')
+                            if(k=='中国普通话'):
+                                language_info['mandarin']=v
+                            elif(k=='英语'):
+                                language_info['english']=v
+                            elif(k=='客家方言'):
+                                language_info['hakka']=v
+                            else:
+                                raise Exception('语言能力-有未处理的字段，地址：{0}'.format(self.previewUrl.format(data["user_id"])))
+                        preview['language_info']=language_info
                     elif(title=='技能与特长'):
-                        pass
+                        special_skills=[]
+                        skills=main.xpath('ul')
+                        if(len(skills)<=0):
+                            continue
+                        for skill in skills:
+                            s={}
+                            skill_li=skill.xpath('li')
+                            if(len(skill_li)<=0):
+                                continue
+                            for s_li in skill_li:
+                                skill_info=s_li.xpath('ul/li/text()')
+                                if(len(skill_info)>0):
+                                    for i in skill_info:
+                                        k,v=i.replace(' ','').replace('：',':').split(':')
+                                        if(k=='特长名称'):
+                                            s['name']=v
+                                        if(k=='掌握程度'):
+                                            s['level']=v
+                                else:
+                                    k,v=s_li.xpath('string(.)').replace(' ','').replace('：',':').split(':')
+                                    if(k=='描述'):
+                                        s['infos']=v
+                            special_skills.insert(0,s)
+                        preview['special_skills']=special_skills
                     elif(title=='教育经历'):
-                        pass
+                        education_experience=[]
+                        experiences=main.xpath('table/tr')
+                        if(len(experiences)<=0):
+                            continue
+                        for experience in experiences:
+                            e={}
+                            ex=experience.xpath('td/text()')
+                            e['date']=ex[0].replace(' ','')
+                            e['city']=ex[1].replace(' ','')
+                            e['school']=ex[2].replace(' ','')
+                            e['major']=ex[3].replace(' ','').replace(u'\xa0\xa0',u'-')
+                            e['education']=ex[4].replace(' ','')
+                            education_experience.insert(0,e)
+                        preview['education_experience']=education_experience
+                    elif(title=='培训经历'):
+                        train_experience=[]
+                        train_ul=main.xpath('ul')
+                        if(len(train_ul)<=0):
+                            continue
+                        for t_ul in train_ul:
+                            train={}
+                            train_info=t_ul.xpath('li/strong/text()')[0]
+                            info_list=train_info.split(' ')
+                            while '' in info_list:
+                                info_list.remove('')
+                            if(len(info_list)>0):
+                                train['date']=info_list[0]+info_list[1]
+                                train['city']=info_list[2]
+                                train['title']=info_list[3]
+
+                            resumes_detail=t_ul.xpath('li/div/text()')
+                            if(len(resumes_detail)>0):
+                                train['resumes_detail']=resumes_detail
+
+                            train_li=t_ul.xpath('li/text()')
+                            while ' ' in train_li:
+                                train_li.remove(' ')
+                            if(len(train_li)<=0):
+                                continue
+                            for t_li in train_li:
+                                k,v=t_li.replace(' ','').replace('：',':').split(':')
+                                if(k=='描述'):
+                                    train['description']=v
+                                elif(k=='培训机构'):
+                                    train['training_agency']=v
+                                elif(k=='获得证书'):
+                                    train['credentials']=v
+                                else:
+                                    raise Exception('培训经历-有未处理的字段，地址：{0}'.format(self.previewUrl.format(data["user_id"])))
+                            train_experience.insert(0,train)
+                        preview['train_experience']=train_experience
+                    elif(title=='证书'):
+                        certificates=[]
+                        certificate=main.xpath('ul')
+                        if(len(certificate)<=0):
+                            continue
+                        for c_ul in certificate:
+                            cret={}
+                            cret_info=c_ul.xpath('li/strong/text()')[0].split(' ')
+                            if(len(cret_info)>0):
+                                cret['date']=cret_info[0]
+                                cret['title']=cret_info[1]
+
+                            resumes_detail=c_ul.xpath('li/div/text()')
+                            if(len(resumes_detail)>0):
+                                cret['resumes_detail']=resumes_detail[0]
+
+                            cret_li=c_ul.xpath('li/a/@href')
+                            cret['cret_images']=cret_li[0]
+
+                            certificates.insert(0,cret)
+                        preview['certificates']=certificates
+                    elif(title=='其他'):
+                        other=[]
+                        other_ul=main.xpath('ul')
+                        if(len(other_ul)<=0):
+                            continue
+                        for o_ul in other_ul:
+                            o={}
+                            title=o_ul.xpath('li/h2/text()')
+                            content=o_ul.xpath('li/text()')
+                            if(len(title)>0 and title[0]=='自我评价'):
+                                o['self_evaluation']=content[0]
+                            other.insert(0,o)
+                        preview['other']=other
+                    elif(title=='附件'):
+                        enclosures=[]
+                        image_list=main.xpath('ul/li/a')
+                        if(len(image_list)<=0):
+                            continue
+                        for image in image_list:
+                            img={}
+                            title=image.xpath('text()')[0]
+                            href=image.xpath('@href')[0]
+                            if(title=='生活照'):
+                                img['live_image']=href
+                            enclosures.insert(0,img)
+                        preview['enclosures']=enclosures
+                print(preview)
         finally:
             cursor.close()
